77e730abf2b36e00f75f50acb78a57af9e7e9493
Vegard Edvardsen <evegard@hotmail.com>
2012-03-01 15:05:26 +0100

    Continued coding ex2

--- ex2/code/ex2.c
+++ ex2/code/ex2.c
@@ -4,6 +4,9 @@
  *
  *****************************************************************************/
 
+#include <stdlib.h>
+#include <math.h>
+
 #include "ex2.h"
 
 volatile avr32_pio_t *piob = &AVR32_PIOB;
@@ -65,18 +68,41 @@ void initLeds(void)
 }
 
 void initAudio(void) {
-  register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
-  /* (...) */
+    register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
+
+    /* Set up PIO for use by ABDAC. */
+
+    /* Disable pins 20 and 21 on PIOB. */
+    piob->PDR.p20 = 1;
+    piob->PDR.p21 = 1;
+
+    /* Grant control of those pins to peripheral A. */
+    piob->ASR.p20 = 1;
+    piob->ASR.p21 = 1;
+
+    /* Set up power manager for use by ABDAC. */
+    volatile avr32_pm_t *pm = &AVR32_PM;
+    pm->GCCTRL[6].diven = 0;
+    pm->GCCTRL[6].pllsel = 0;
+    pm->GCCTRL[6].oscsel = 1;
+    pm->GCCTRL[6].cen = 1;
+
+    /* Set up the ABDAC. */
+    volatile avr32_abdac_t *dac = &AVR32_ABDAC;
+    dac->CR.en = 1;
+    dac->IER.tx_ready = 1;
 }
 
 void button_isr(void)
 {
+    /* Do debouncing before reading the data pins. */
+    volatile int k = 1000;
+    while (k--);
+
     volatile avr32_pio_t *pio = &AVR32_PIOB;
     int isr = pio->isr,
         pdsr = pio->pdsr;
     int i, j;
-    volatile int k = 0;
-    while (k < 1000) k++;
     for (i = 0; i < 8; i++) {
         if ((isr & (1 << i)) && (~pdsr & (1 << i))) {
             if (i == 0) {
@@ -100,4 +126,20 @@ void button_isr(void)
     }
 }
 
-void abdac_isr(void) {}
+int step = 0;
+int rate = 46875 >> 3;
+int amp = 65535;
+int freq = 440;
+
+void abdac_isr(void)
+{
+    volatile avr32_abdac_t *dac = &AVR32_ABDAC;
+    //dac->SDR.channel0 = (short)(sin(step * 2 * 3.14 * freq / rate) * amp);
+    //dac->SDR.channel1 = (short)(sin(step * 2 * 3.14 * freq / rate) * amp);
+    dac->SDR.channel0 = (short)(step < rate/2 ? 1 : 0)*10000;
+    dac->SDR.channel0 = (short)(step < rate/2 ? 1 : 0)*10000;
+
+    step++;
+    if (step >= rate)
+        step = 0;
+}
